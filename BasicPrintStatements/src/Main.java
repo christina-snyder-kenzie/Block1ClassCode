3e8a461a26faaaeb366f804343162bba372a3945
christina-snyder-kenzie <christina.snyder@kenzie.academy>
2022-08-09 13:00:58 -0400

    Basic print statements Basic math in println

---
+++ BasicPrintStatements/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        System.out.println(1992 + 2022);
+        System.out.println(2*3);
+        System.out.println(10      -     7);
+        System.out.println(14 / 5);
+        System.out.println(14 / 5.0);
+
+        // int vs double
+
+        //int -> whole numbers
+
+        //integer division -> Always whole numbers, answer is TRUNCATED
+
+        //truncation -> chopping off the decimal
+    }
+}
