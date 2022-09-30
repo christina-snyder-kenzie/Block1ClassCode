8c01c7b2633d5a2353ac58ae09a2da0042e3891e
christina-snyder-kenzie <christina.snyder@kenzie.academy>
2022-09-30 09:51:49 -0400

    Warmup now included

---
+++ warmup-27Sept.java
@@ -0,0 +1,31 @@
+int[] arr = {7, 5, 1, 3, 8, 2};
+			{2, 8, 3, 1, 4, 7};
+
+//looping directions
+//forwards vs backwards
+
+//forwards
+for (int i = 0; i < arr.length; i++){
+	int element = arr[i];
+}
+
+//backwards
+for (int i = arr.length - 1; i >= 0; i--){
+	int element = arr[i];
+}
+
+//can I access elements in reverse order, while loop FORWARDS
+for (int i = 0; i < arr.length; i++){
+	int element = arr[arr.length - 1 - i];
+}
+
+//reverse an array
+//perform SWAPS
+int temp = arr[pos1];
+arr[pos1] = arr[pos2];
+arr[pos2] = temp;
+
+
+
+
+
